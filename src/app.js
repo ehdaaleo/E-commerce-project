1f18add5536b8d642dfc95a77b181700021212fb
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-02-17 01:58:13 +0200

    feature/auth

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
-import express from "express";
-import connectDB from "./config/db.js";
+import express from 'express';
+import connectDB from './config/db.js';
+import router from './routes/authRoutes.js';
+
 export const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'welcome to our website',
+    });
+});
+
+app.get('/home', (req, res) => {
+    res.send({
+        message: 'welcome to home our website',
+    });
+});
+
+app.use('/auth', router);
+
 const startServer = async () => {
-  await connectDB();
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(
+            'Server is running on port 3000, click here: http://localhost:' +
+                PORT
+        );
+    });
 };
 
 startServer();
---
+++ src/controllers/authController.js
@@ -0,0 +1,121 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { validateSignin, validateSignup } from '../middleware/validation.js';
+
+export const signup = async (req, res) => {
+    try {
+        const valid = validateSignup(req.body);
+
+        if (!valid) {
+            return res.status(400).json({
+                timestamp: new Date(),
+                success: false,
+                errors: validateSignup.errors,
+            });
+        }
+
+        const { name, email, password, phone, address } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                timestamp: new Date(),
+                success: false,
+                message: 'Email already exists',
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            phone,
+            address,
+        });
+
+        res.status(201).json({
+            timestamp: new Date(),
+            success: true,
+            message: `Welcome to out website ${user.name}`,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            timestamp: new Date(),
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const signin = async (req, res) => {
+    try {
+        const valid = validateSignin(req.body);
+
+        if (!valid) {
+            return res.status(400).json({
+                timestamp: new Date(),
+
+                success: false,
+                errors: validateSignin.errors,
+            });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                timestamp: new Date(),
+                success: false,
+                message: 'Invalid Email or password',
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                timestamp: new Date(),
+
+                success: false,
+                message: 'Invalid Email or password',
+            });
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '2d' }
+        );
+
+        res.status(200).json({
+            timestamp: new Date(),
+            success: true,
+            message: `Welcome back ${user.name}`,
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            timestamp: new Date(),
+            success: false,
+            message: error.message,
+        });
+    }
+};
