fc4c84568b1c55252c78ea50b2d371365eeda051
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-02-17 09:16:17 +0200

    change branch

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
     res.send({
-        message: 'welcome to home our website',
+        message: 'welcome to our home website ',
     });
 });
 
