6f68a3b67d0b3963c960cd14ded4af54e6e5615e
Omar Abdelatstar <omarabdelstar2002@gmail.com>
2026-02-21 16:01:18 +0200

    adding email confirmation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import wishlistRoutes from './routes/wishlistRoutes.js';
-
-import reviewRoutes from './routes/reviewRoutes.js';
-import promoRoutes from './routes/promoRoutes.js';
-import paymentRoutes from './routes/paymentRoutes.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import wishlistRoutes from "./routes/wishlistRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import promoRoutes from "./routes/promoRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 
 export const app = express();
 app.use(express.json());
@@ -19,38 +18,36 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send({
-        message: 'welcome to our website',
-    });
+app.get("/", (req, res) => {
+  res.send({
+    message: "welcome to our website",
+  });
 });
 
-app.get('/home', (req, res) => {
-    res.send({
-        message: 'welcome to home our website',
-    });
+app.get("/home", (req, res) => {
+  res.send({
+    message: "welcome to home our website",
+  });
 });
 
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/carts', cartRoutes);
-
-app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/wishlists', wishlistRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/payment', paymentRoutes);
-app.use('/api/promos', promoRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/carts", cartRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/wishlists", wishlistRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/promos", promoRoutes);
+app.use("/payment", paymentRoutes);
 
 const startServer = async () => {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log(
-            'Server is running on port 3000, click here: http://localhost:' +
-                PORT
-        );
-    });
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(
+      "Server is running on port 3000, click here: http://localhost:" + PORT,
+    );
+  });
 };
 
 startServer();
